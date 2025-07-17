5296e69f50d6b8fe0e72843c7ce46618ffd74c4e
Azriel84ht <el.sirviente.de.los.huesos@gmail.com>
2025-07-18 00:04:20 +0200

    fix(backend): Implementar política de CORS para permitir peticiones del web-client

--- apps/auth-service/src/server.js
+++ apps/auth-service/src/server.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const passport = require('passport'); // <-- AÑADIDO
+const passport = require('passport');
+const cors = require('cors'); // <-- AÑADIDO
 const { testConnection } = require('./config/db');
 const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// --- INICIO DE CONFIGURACIÓN DE CORS ---
+const corsOptions = {
+  origin: 'http://localhost:8080', // Permite solo peticiones desde el cliente web
+  credentials: true, // Permite el envío de cookies
+};
+app.use(cors(corsOptions));
+// --- FIN DE CONFIGURACIÓN DE CORS ---
+
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
 
 // Configuración de Passport
-app.use(passport.initialize()); // <-- AÑADIDO
-require('./config/passport-setup'); // <-- AÑADIDO (Ejecuta la configuración de la estrategia)
+app.use(passport.initialize());
+require('./config/passport-setup');
 
 // Probar conexión a la BD al iniciar
 testConnection();
